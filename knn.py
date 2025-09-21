14ca9d67323ef71bb2f4edc53d5024ecaa9aca10
kaavya-png <kthillai@unc.edu>
2025-09-21 00:22:16 -0400

    finished descion trees

--- knn.py
+++ knn.py
@@ -4,26 +4,80 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import os
-from sklearn.neighbors import KNeighborsClassifier
-#from sklearn.datasets import load_iris
+
+
 
 data = list()
 
-
+#formatting the dataset
 with open('Pop_Culture.csv','r') as csvfile:
     csvreader = csv.reader(csvfile)
     header = next(csvreader)
     for row in csvreader:
         data.append(row)
 
-
+#converting to dataframe
 data_df = pd.DataFrame(data)
+
+#naming columns
 data_df.columns = ['review_id','fav_heroe','fav_villain',"fav_film","fav_soundtrack", "fav_spaceship", "fav_planet", "fav_robot"]
-#print(data_df)
+
+#making x and y and preprocessing data
+from sklearn.preprocessing import OneHotEncoder
+
 x = data_df[["fav_film", "fav_soundtrack", "fav_spaceship", "fav_planet", "fav_robot"]]
-y = data_df[["fav_heroe"]]
+encoded = OneHotEncoder()
+x_mod = encoded.fit_transform(x)
+y = data_df["fav_heroe"]
 
-knn = KNeighborsClassifier(n_neighbors=5)
+#splitting dataset and training
+from sklearn.model_selection import train_test_split
 
-knn.fot(x,y)
+x_train, x_test, y_train, y_test = train_test_split(x_mod, y, test_size = 0.2, random_state=60)
+
+#testing data
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+mL = DecisionTreeClassifier(max_depth=18, random_state=42)
+mL.fit(x_train, y_train)
+
+y_pred = mL.predict(x_test)
+#print(accuracy_score(y_test, y_pred))
+
+# change dashes based on what the user input is
+user_input = pd.DataFrame([{
+    "fav_film": "---"
+    "fav_soundtrack": "---"
+    "fav_spaceship": "---"
+    "fav_planet": "---"
+    "fav_robot": "---" 
+    }])
+
+#this ranks the probabilities of matches based on user responses
+user_mod = encoded.fit_transform(user_input)
+prediction = mL.predict(user_mod)
+
+probability = (mL.predict_proba(user_input) * 100)
+match = mL.classes_
+match = match
+
+sample_index = 0
+sample_probs = probability[0]
+
+ranking = pd.DataFrame({ 
+    "Match": match,
+    "Probability" : sample_probs
+}).sort_values("Probability", ascending=False)
+
+print(ranking)
+
+#from sklearn.neighbors import KNeighborsClassifier
+
+#knn = KNeighborsClassifier(n_neighbors=10)
+#knn.fit(x_train, y_train)
+
+#print(knn.score(x_test, y_test))
+
+
 
